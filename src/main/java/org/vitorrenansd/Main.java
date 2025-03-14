91f5a6e02c0a38c10b265891729b89881248f813
VitorRenanSD <vitorrenansouzadias@gmail.com>
2025-03-14 15:22:56 -0300

    Update Main.java

--- src/main/java/org/vitorrenansd/Main.java
+++ src/main/java/org/vitorrenansd/Main.java
@@ -3,7 +3,7 @@ package org.vitorrenansd;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        System.out.printf("teste");
 
     }
 }
