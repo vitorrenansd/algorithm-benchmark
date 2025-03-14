7c29e90eaefe1b11a6764c8daa5bc68a765cc2bb
VitorRenanSD <vitorrenansouzadias@gmail.com>
2025-03-14 16:55:21 -0300

    Initial commit

--- src/main/java/benchmark/classes/Algorithms.java
+++ src/main/java/benchmark/classes/Algorithms.java
@@ -2,4 +2,17 @@ package benchmark.classes;
 
 public class Algorithms {
     
+
+    public void mergeSort() {
+
+    }
+
+    public void bubbleSort() {
+
+    }
+
+    public void quickSort() {
+
+    }
+    
 }
