e7044663d0fe337c810d70e3b0c6a44c8cd8a0bf
VitorRenanSD <vitorrenansouzadias@gmail.com>
2025-03-14 16:34:00 -0300

    Resolving paths

--- src/main/java/benchmark/Main.java
+++ src/main/java/benchmark/Main.java
@@ -1,4 +1,4 @@
-package org.vitorrenansd;
+package benchmark;
 
 public class Main {
     public static void main(String[] args) {
--- src/main/java/vitorrenansd/classes/Algorithms.java
+++
@@ -1,5 +0,0 @@
-package org.vitorrenansd.classes;
-
-public class Algorithms {
-    
-}
