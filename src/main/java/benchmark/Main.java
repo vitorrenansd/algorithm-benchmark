86386ed0b3ceb8fc584051b0ccfda07af4e20bdc
VitorRenanSD <vitorrenansouzadias@gmail.com>
2025-03-15 10:45:15 -0300

    Creating bubbleSort() method

--- src/main/java/benchmark/Main.java
+++ src/main/java/benchmark/Main.java
@@ -1,7 +1,13 @@
 package benchmark;
 
+import benchmark.classes.SortingAlgorithms;
+
 public class Main {
     public static void main(String[] args) {
+        SortingAlgorithms so = new SortingAlgorithms();
 
+        int[] values = {8, 9, 2, 1, 8, 17, 0, 5, 11};
+
+        so.bubbleSort(values);
     }
 }
--- src/main/java/benchmark/classes/SortingAlgorithms.java
+++ src/main/java/benchmark/classes/SortingAlgorithms.java
@@ -1,13 +1,36 @@
 package benchmark.classes;
 
+import java.util.Arrays;
+
 public class SortingAlgorithms {
     
 
-    public void mergeSort() {
-
+    public SortingAlgorithms() {
     }
 
-    public void bubbleSort() {
+    public void bubbleSort(int[] array) {
+        // Create a copy of the original array to avoid modifying the input
+        int[] arr = Arrays.copyOf(array, array.length);
+
+        // Iterate through the array (n - 1) times
+        for (int i = 0; i < arr.length - 1; i++) {
+            
+            // Traverse the array up to the last unsorted position
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                
+                // Swap values if the current one is greater than the next
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+
+    public void mergeSort() {
 
     }
 
