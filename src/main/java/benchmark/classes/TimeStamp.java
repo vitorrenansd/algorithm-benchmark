fea2aebe477b4e5b7a46094e33df5924525dee93
Vitor RenanSD <vitorrenansouzadias@gmail.com>
2025-03-20 12:06:44 -0300

    Update TimeStamp.java

--- src/main/java/benchmark/classes/TimeStamp.java
+++ src/main/java/benchmark/classes/TimeStamp.java
@@ -1,52 +1,29 @@
 package benchmark.classes;
 
-import benchmark.algorithms.*;
-
 public class TimeStamp {
-    public void benchBubbleSorting(int[] values) {
-        BubbleSort bs = new BubbleSort();
+    public int testsQuantity;
 
+    // Constructor method
+    public TimeStamp(int testsQuantity) {
+        testsQuantity = this.testsQuantity;
+    }
+
+    // Benchmark, iterate N times and print the avg time
+    public void benchSorting(int[] values) {
+
+        for (int i = 0; i < testsQuantity; i++) {
+
+        }
         // Start measuring
         long startTime = System.nanoTime();
 
-        bs.bubbleSort(values);
+
 
         // Stop measuring
         long endTime = System.nanoTime();
 
-        // Calculate the execution time
         long executionTime = (endTime - startTime);
-
-        System.out.println("Execution time for BubbleSort: " + executionTime + "ns. " + (executionTime / 1000000) + "ms.");
+        System.out.println("Execution time for: " + executionTime + "ns. " + (executionTime / 1000000) + "ms.");
     }
 
-    public void benchMergeSorting(int[] values) {
-        
-        MergeSort ms = new MergeSort();
-
-        long startTime = System.nanoTime();
-
-        ms.mergeSort(values);
-
-        long endTime = System.nanoTime();
-
-        long executionTime = (endTime - startTime);
-
-        System.out.println("Execution time for MergeSort: " + executionTime + "ns. " + (executionTime / 1000000) + "ms.");
-    }
-
-    public void benchHeapSorting(int[] values) {
-        
-        HeapSort hp = new HeapSort();
-
-        long startTime = System.nanoTime();
-
-        hp.heapSort(values);
-
-        long endTime = System.nanoTime();
-
-        long executionTime = (endTime - startTime);
-
-        System.out.println("Execution time for HeapSort: " + executionTime + "ns. " + (executionTime / 1000000) + "ms.");
-    }
 }
