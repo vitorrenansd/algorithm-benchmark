0fbe441ffffb0576669b114386de71ea468dcf72
Vitor RenanSD <vitorrenansouzadias@gmail.com>
2025-03-20 11:56:34 -0300

    Update Main.java

--- src/main/java/benchmark/Main.java
+++ src/main/java/benchmark/Main.java
@@ -2,11 +2,8 @@ package benchmark;
 
 import java.util.Random;
 
-import benchmark.classes.TimeStamp;
-
 public class Main {
     public static void main(String[] args) {
-        TimeStamp ts = new TimeStamp();
 
         // Gera 1500 valores entre -10000 e 10000
         int[] values = new int[1500];
@@ -14,9 +11,6 @@ public class Main {
         for (int i = 0; i < values.length; i++) {
             values[i] = rand.nextInt(20001) - 10000; 
         }
-        
-        ts.benchMergeSorting(values);
-        ts.benchBubbleSorting(values);
-        ts.benchHeapSorting(values);
+
     }
 }
