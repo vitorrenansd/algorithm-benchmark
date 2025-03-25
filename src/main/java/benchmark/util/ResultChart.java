7212ed03c96149c73717e71fd17224db35e7e37f
Vitor RenanSD <vitorrenansouzadias@gmail.com>
2025-03-25 18:35:44 -0300

    Initial commit ResultChart class

--- src/main/java/benchmark/util/ResultChart.java
+++ src/main/java/benchmark/util/ResultChart.java
@@ -1,5 +1,14 @@
 package benchmark.util;
 
-public class ResultChart {
+import javax.swing.JFrame;
 
+public class ResultChart extends JFrame{
+
+    public ResultChart() {
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setTitle("benchmark-algorithm");
+        setSize(800, 600); // 800x600 resolution
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
 }
