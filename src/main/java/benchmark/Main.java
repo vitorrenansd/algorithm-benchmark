6b0a71bc4bdaa14d845cbb7b83178a33aacd1776
Vitor RenanSD <vitorrenansouzadias@gmail.com>
2025-03-26 16:56:56 -0300

    BarGraph tests

--- src/main/java/benchmark/Main.java
+++ src/main/java/benchmark/Main.java
@@ -4,10 +4,7 @@ import benchmark.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        ImageExtractor ie = new ImageExtractor();
 
-        // Now, returns a List<Long>
-        System.out.println(ie.pullAllLastModified("images"));
-        System.out.println(ie.pullAllSize("images"));
+        new ResultChart();
     }
 }
