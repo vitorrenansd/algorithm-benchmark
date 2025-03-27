44f0f2f75914671cf821435c309bcf59f48e7453
VitorRenanSD <vitorrenansouzadias@gmail.com>
2025-03-27 20:24:03 -0300

    HeapSort using now List<Long>

--- src/main/java/benchmark/Main.java
+++ src/main/java/benchmark/Main.java
@@ -1,13 +1,24 @@
 package benchmark;
 
-import benchmark.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 
 public class Main {
     public static void main(String[] args) {
-        ImageExtractor ie = new ImageExtractor();
+        
 
-        // Now, returns a List<Long>
-        System.out.println(ie.pullAllLastModified("images"));
-        System.out.println(ie.pullAllSize("images"));
+        int size = 1500;
+        List<Long> testList = new ArrayList<>(size);
+        Random random = new Random();
+        
+        for (int i = 0; i < size; i++) {
+            long randomNum = random.nextLong();
+            randomNum = randomNum - (random.nextBoolean() ? 0 : 5000);
+            testList.add(randomNum);
+        }
+
+
     }
 }
