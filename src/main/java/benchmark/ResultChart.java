6e4a5f4d845144a4dc16a3a8875d6c685d0ea277
Vitor RenanSD <vitorrenansouzadias@gmail.com>
2025-04-02 13:33:09 -0300

    Deleting method, transfering to benchmark.application.Frame

--- src/main/java/benchmark/ResultChart.java
+++ src/main/java/benchmark/ResultChart.java
@@ -10,17 +10,6 @@ import org.jfree.data.category.DefaultCategoryDataset;
 
 public class ResultChart extends JFrame{
 
-    public ResultChart() {
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setTitle("benchmark-algorithm");
-        setSize(800, 600); // 800x600 resolution
-        setLocationRelativeTo(null);
-
-        createBarGraph(489,58,899);
-
-        setVisible(true);
-    }
-
     public void createBarGraph(Integer mergeTime, Integer heapTime, Integer bubbleTime) {
             DefaultCategoryDataset bar = new DefaultCategoryDataset();
 
@@ -30,7 +19,9 @@ public class ResultChart extends JFrame{
             bar.setValue(bubbleTime, "BubbleSort", "");
 
             // Graph config
-            JFreeChart graph = ChartFactory.createBarChart("Tempo necessário (ms)", "", "ms", bar, PlotOrientation.VERTICAL, true, true, false);
+            JFreeChart graph = ChartFactory.createBarChart("Tempo necessário (ms)", "", "ms", bar, PlotOrientation.HORIZONTAL, true, true, false);
+
+            // Graph add to panel
             ChartPanel panel = new ChartPanel(graph);
             add(panel);
     }
