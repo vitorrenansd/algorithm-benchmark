70246de40410ec506cdf5b1f0245f15d15a59dd0
Vitor RenanSD <vitorrenansouzadias@gmail.com>
2025-04-02 13:12:20 -0300

    Create Panel.java

---
+++ src/main/java/benchmark/application/Panel.java
@@ -0,0 +1,7 @@
+package benchmark.application;
+
+import javax.swing.JPanel;
+
+public class Panel extends JPanel{
+    // Aqui ficará o Painel da aplicação, seriam todos os botoes, funções de tela, etc
+}
