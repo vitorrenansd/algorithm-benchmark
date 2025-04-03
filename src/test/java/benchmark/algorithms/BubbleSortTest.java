6a1ea3954c2fc17e3b9afda588190e2231682b08
Vitor RenanSD <vitorrenansouzadias@gmail.com>
2025-04-03 11:56:11 -0300

    Create BubbleSortTest.java

---
+++ src/test/java/benchmark/algorithms/BubbleSortTest.java
@@ -0,0 +1,21 @@
+package benchmark.algorithms;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BubbleSortTest {
+
+    @Test
+    void validSort() {
+        BubbleSort bubbleSort = new BubbleSort();
+        List<Long> values = new ArrayList<>(List.of(8L, 2L, 1L, 4L, 0L));
+        List<Long> expected = new ArrayList<>(List.of(0L, 1L, 2L, 4L, 8L));
+
+        List<Long> sortingResult = bubbleSort.sort(values);
+
+        assertEquals(expected, sortingResult, "List should be sorted in ascending order");
+    }
+}
