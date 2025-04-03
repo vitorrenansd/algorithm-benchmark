50f439f73590903f1777b0c1db939d06ed46df73
Vitor RenanSD <vitorrenansouzadias@gmail.com>
2025-04-03 10:28:25 -0300

    Create ImageExtractorTest.java

---
+++ src/test/java/benchmark/application/ImageExtractorTest.java
@@ -0,0 +1,49 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.io.IOException;
+
+import benchmark.application.ImageExtractor;
+
+public class ImageExtractorTest {
+
+    @Test
+    void pullAllLastModifiedValidPath() throws IOException {
+        // Creates a temp directory with 1 file to run this test
+        Path tempDir = Files.createTempDirectory("validPath");
+        Files.createFile(tempDir.resolve("testfile.txt"));
+
+        ImageExtractor ie = new ImageExtractor();
+        ie.setImagesPath(tempDir.toString());
+
+        assertFalse(ie.pullAllLastModified().isEmpty(), "The list should not be empty for a valid directory with files.");
+    }
+
+    @Test
+    void pullAllLastModifiedInvalidPath() {
+        ImageExtractor ie = new ImageExtractor();
+        ie.setImagesPath("invalid\\path\\that\\does\\not\\exist");
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            ie.pullAllLastModified();
+        });
+
+        assertEquals("Error: The directory doesn't exist or is not valid.", exception.getMessage());
+    }
+
+    @Test
+    void pullAllLastModifiedNoFilesFound() throws IOException {
+        ImageExtractor ie = new ImageExtractor();
+        // Creates a temp directory without files to run this test
+        Path tempDir = Files.createTempDirectory("validPath");
+        ie.setImagesPath(tempDir.toString());
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            ie.pullAllLastModified();
+        });
+        
+        assertEquals("No files found in the directory.", exception.getMessage());
+    }
+}
