2ed373fc0a2f023b29df13cde17f2e2dc47c7895
Vitor RenanSD <vitorrenansouzadias@gmail.com>
2025-04-03 12:22:49 -0300

    Merge branch 'main' into algorithm-unit-tests

--- src/main/java/benchmark/algorithms/BubbleSort.java
+++ src/main/java/benchmark/algorithms/BubbleSort.java
@@ -9,6 +9,9 @@ import java.util.ArrayList;
 public class BubbleSort implements SortingAlgorithm {
     @Override
     public List<Long> sort(List<Long> array) {
+        if (array.size() == 0) {
+            throw new IllegalArgumentException("Original list can't be empty");
+        }
         // Create a copy of the original array
         List<Long> arr = new ArrayList<>(array);
         
