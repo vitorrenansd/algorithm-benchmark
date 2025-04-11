a0b78ea6665648bd55f029ce997b4fecc9147067
Vitor RenanSD <vitorrenansouzadias@gmail.com>
2025-04-11 11:57:21 -0300

    Update Window.java

--- src/main/java/benchmark/sysinterface/Window.java
+++ src/main/java/benchmark/sysinterface/Window.java
@@ -1,46 +1,28 @@
 package benchmark.sysinterface;
 
-import javax.swing.JComboBox;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
+import javax.swing.*;
 
 public class Window extends JFrame {
-    
     public Window() {
+        // Initial window config
+        setTitle("Algorithm Benchmark");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(800, 600); // Resolution 800x600
+        setLocationRelativeTo(null); // Center
+        setResizable(false);
+
+        // Creating components
         String[] algorithms = {"File datetime", "File size (bytes)", "The two's"};
         JLabel chooseAlgorithmLabel = new JLabel("Choose datatype:");
         JComboBox<String> chooseAlgorithm = new JComboBox<>(algorithms);
 
-        chooseAlgorithmLabel.setBounds(10, 10, 150, 15);
-        chooseAlgorithm.setSelectedIndex(0);
-        chooseAlgorithm.setMaximumRowCount(3);
-        chooseAlgorithm.setBounds(10, 30, 130, 25);
-        add(chooseAlgorithmLabel);
-        add(chooseAlgorithm);
-
         JLabel folderPathLabel = new JLabel("Images path:");
         JTextField folderPath = new JTextField();
-
-        folderPathLabel.setBounds(180, 10, 100, 15);
-        folderPath.setBounds(180, 30, 470, 25);
-        add(folderPathLabel);
-        add(folderPath);
 
         JLabel quantityLabel = new JLabel("Tests quantity:");
         JTextField quantity = new JTextField();
 
-        quantityLabel.setBounds(680, 10, 100, 15);
-        quantity.setBounds(680, 30, 90, 25);
-        add(quantityLabel);
-        add(quantity);
 
-
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLayout(null);
-        setSize(800, 600); // Resolution 800x600
-        setLocationRelativeTo(null); // Center
-        setResizable(false);
         setVisible(true);
     }
 }
