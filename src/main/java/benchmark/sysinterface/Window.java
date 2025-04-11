682aa983c6eefe81af9ff01f9e0c160ccc38594a
Vitor RenanSD <vitorrenansouzadias@gmail.com>
2025-04-11 15:05:47 -0300

    Horizontal and vertical layouts

--- src/main/java/benchmark/sysinterface/Window.java
+++ src/main/java/benchmark/sysinterface/Window.java
@@ -1,6 +1,7 @@
 package benchmark.sysinterface;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Window extends JFrame {
     public Window() {
@@ -13,24 +14,59 @@ public class Window extends JFrame {
 
         // Creating components
         // First collumn (config)
-        String[] algorithms = {"File datetime", "File size (bytes)", "The two's"};
+        String[] algorithms = {"Datetime", "Size (bytes)", "The two's"};
         JLabel chooseAlgorithmLabel = new JLabel("Choose datatype:");
         JComboBox<String> chooseAlgorithm = new JComboBox<>(algorithms);
+        chooseAlgorithm.setPreferredSize(new Dimension(150, 25));
 
         JLabel folderPathLabel = new JLabel("Images path:");
         JTextField folderPath = new JTextField();
+        folderPath.setPreferredSize(new Dimension(500, 25));
 
         JLabel quantityLabel = new JLabel("Tests quantity:");
         JTextField quantity = new JTextField();
+        quantity.setPreferredSize(new Dimension(100, 25));
 
         // Second column (big window benchmark)
-
-
 
         // Third collumn (especific algorithm buttons)
 
 
+        // Creating layout
+        JPanel panel = new JPanel();
+        GroupLayout layout = new GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
 
+        layout.setAutoCreateGaps(true);
+        layout.setAutoCreateContainerGaps(true);
+
+        // Setting layout to application
+        layout.setHorizontalGroup(
+            layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                    .addComponent(chooseAlgorithmLabel)
+                    .addComponent(chooseAlgorithm, GroupLayout.PREFERRED_SIZE, 150, GroupLayout.PREFERRED_SIZE))
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                    .addComponent(folderPathLabel)
+                    .addComponent(folderPath, GroupLayout.PREFERRED_SIZE, 500, GroupLayout.PREFERRED_SIZE))
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                    .addComponent(quantityLabel)
+                    .addComponent(quantity, GroupLayout.PREFERRED_SIZE, 100, GroupLayout.PREFERRED_SIZE))
+        );
+
+        layout.setVerticalGroup(
+            layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(chooseAlgorithmLabel)
+                    .addComponent(folderPathLabel)
+                    .addComponent(quantityLabel))
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(chooseAlgorithm)
+                    .addComponent(folderPath)
+                    .addComponent(quantity))
+        );
+
+        add(panel);
         setVisible(true);
     }
 }
