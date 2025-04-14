656a40e4dfd66f7b55e86115ac06d1251b15bfda
Vitor RenanSD <vitorrenansouzadias@gmail.com>
2025-04-14 15:10:42 -0300

    Add Javadoc documentation for SortingAlgorithm

--- src/main/java/benchmark/application/SortingAlgorithm.java
+++ src/main/java/benchmark/application/SortingAlgorithm.java
@@ -2,6 +2,21 @@ package benchmark.application;
 
 import java.util.List;
 
+/**
+ * The SortingAlgorithm interface defines a contract for sorting algorithms.
+ * Any class implementing this interface must provide an implementation of the
+ * sort() method to sort a list of Long values.
+ *
+ * <p>Example usage:</p>
+ * <pre>
+ *     SortingAlgorithm bubbleSort = new BubbleSort();
+ *     List<Long> sortedValues = bubbleSort.sort(values);
+ * </pre>
+ *
+ * @author vitorrenansd
+ * @version 0.7.2-beta
+ * @since 2025-04-14
+ */
 public interface SortingAlgorithm {
     List<Long> sort(List<Long> values); // Default method for all algorithms
 }
