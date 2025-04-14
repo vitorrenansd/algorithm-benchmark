aa6e8f9ae3f6c8317f10a0df08be6038a9497860
Vitor RenanSD <vitorrenansouzadias@gmail.com>
2025-04-14 14:21:18 -0300

    Create BenchmarkPanel.java

---
+++ src/main/java/benchmark/sysinterface/BenchmarkPanel.java
@@ -0,0 +1,12 @@
+package benchmark.sysinterface;
+
+import javax.swing.*;
+import java.awt.*;
+
+// PLACEHOLDER, THIS SHIT DOES NOTHING
+public class BenchmarkPanel extends JPanel {
+    public BenchmarkPanel() {
+        setPreferredSize(new Dimension(600, 300));
+        setBackground(Color.GRAY);
+    }
+}
