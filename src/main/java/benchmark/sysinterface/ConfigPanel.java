2e216969a78f65069eb66e50c95d73bbd71a6a57
Vitor RenanSD <vitorrenansouzadias@gmail.com>
2025-04-14 14:31:30 -0300

    Added comments in ConfigPanel

--- src/main/java/benchmark/sysinterface/ConfigPanel.java
+++ src/main/java/benchmark/sysinterface/ConfigPanel.java
@@ -4,25 +4,28 @@ import javax.swing.*;
 import java.awt.*;
 
 public class ConfigPanel extends JPanel {
+    // Attribute == frame Object
     public JComboBox<String> chooseAlgorithm;
     public JTextField folderPath;
     public JTextField quantity;
 
     public ConfigPanel() {
-        setLayout(new FlowLayout(FlowLayout.LEFT));
+        setLayout(new FlowLayout(FlowLayout.CENTER)); // Centralized
 
+        // Algorithms combobox
         String[] algorithms = {"Datetime", "Size (bytes)", "The two's"};
-
         add(new JLabel("Datatype:"));
         chooseAlgorithm = new JComboBox<>(algorithms);
         chooseAlgorithm.setPreferredSize(new Dimension(150, 25));
         add(chooseAlgorithm);
 
+        // Images path field
         add(new JLabel("Images path:"));
         folderPath = new JTextField();
         folderPath.setPreferredSize(new Dimension(460, 25));
         add(folderPath);
 
+        // Quantity field
         add(new JLabel("Tests quantity:"));
         quantity = new JTextField();
         quantity.setPreferredSize(new Dimension(85, 25));
