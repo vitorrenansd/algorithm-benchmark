83e6a2691f8bd7cf00ed0adff58ac8f537d80f30
Vitor Dias <vitorrenansouzadias@gmail.com>
2025-05-09 17:10:00 -0300

    Added tests to pullAllSIze()

--- src/test/java/benchmark/application/ImageExtractorTest.java
+++ src/test/java/benchmark/application/ImageExtractorTest.java
@@ -46,4 +46,43 @@ public class ImageExtractorTest {
         
         assertEquals("No files found in the directory.", exception.getMessage());
     }
+
+
+    @Test
+    void pullAllSizeValidPath() throws IOException {
+        // Creates a temp directory with 1 file to run this test
+        Path tempDir = Files.createTempDirectory("validPath");
+        Files.createFile(tempDir.resolve("testfile.txt"));
+
+        ImageExtractor ie = new ImageExtractor();
+        ie.setImagesPath(tempDir.toString());
+
+        assertFalse(ie.pullAllSize().isEmpty(), "The list should not be empty for a valid directory with files.");
+    }
+
+    @Test
+    void pullAllSizeInvalidPath() {
+        ImageExtractor ie = new ImageExtractor();
+        ie.setImagesPath("invalid\\path\\that\\does\\not\\exist");
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            ie.pullAllSize();
+        });
+
+        assertEquals("Error: The directory doesn't exist or is not valid.", exception.getMessage());
+    }
+
+    @Test
+    void pullAllSizeNoFilesFound() throws IOException {
+        ImageExtractor ie = new ImageExtractor();
+        // Creates a temp directory without files to run this test
+        Path tempDir2 = Files.createTempDirectory("validPath");
+        ie.setImagesPath(tempDir2.toString());
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            ie.pullAllSize();
+        });
+        
+        assertEquals("No files found in the directory.", exception.getMessage());
+    }
 }
