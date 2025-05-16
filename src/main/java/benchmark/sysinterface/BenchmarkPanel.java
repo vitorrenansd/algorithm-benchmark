b9ad2a95b9dad10864cb0d605729acc75e666fe6
Vitor RenanSD <vitorrenansouzadias@gmail.com>
2025-05-16 17:06:03 -0300

    Initial commit

--- src/main/java/benchmark/sysinterface/BenchmarkPanel.java
+++ src/main/java/benchmark/sysinterface/BenchmarkPanel.java
@@ -1,14 +1,17 @@
-// PLACEHOLDER, THIS SHIT DOES NOTHING
-// ITS JUST A GRAY BAKGROUND
+// PLACEHOLDER
 
 package benchmark.sysinterface;
 
 import javax.swing.*;
 import java.awt.*;
+import benchmark.application.ResultChart;
 
 public class BenchmarkPanel extends JPanel {
     public BenchmarkPanel() {
         setPreferredSize(new Dimension(600, 300));
         setBackground(Color.GRAY);
+        
+        ResultChart graph = new ResultChart();
+        graph.createBarGraph(10, 20, 40);
     }
 }
