95d4fb4abdddfd40dc10833e71e52f41b5bdca3b
Vitor RenanSD <vitorrenansouzadias@gmail.com>
2025-05-16 15:21:15 -0300

    Removed specific algorithms button. Only a RUN button

--- src/main/java/benchmark/sysinterface/ButtonPanel.java
+++ src/main/java/benchmark/sysinterface/ButtonPanel.java
@@ -5,19 +5,13 @@ import java.awt.*;
 
 public class ButtonPanel extends JPanel {
     // Attribute == frame Object
-    public JButton bubbleButton;
-    public JButton heapButton;
-    public JButton mergeButton;
+    public JButton benchmarkButton;
 
     public ButtonPanel() {
         setLayout(new FlowLayout(FlowLayout.CENTER, 30, 10)); // Centralized with gap between
 
-        bubbleButton = new JButton("Bubble Sort");
-        heapButton = new JButton("Heap Sort");
-        mergeButton = new JButton("Merge Sort");
+        benchmarkButton = new JButton("Run Benchmark");
 
-        add(bubbleButton);
-        add(heapButton);
-        add(mergeButton);
+        add(benchmarkButton);
     }
 }
