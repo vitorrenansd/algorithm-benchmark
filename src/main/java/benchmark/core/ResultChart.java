d1bf46ef2149b633d985e4b5d259d4de7f23e862
Vitor RenanSD <vitorrenansouzadias@gmail.com>
2025-05-19 13:28:53 -0300

    Added milisecond value information in resultchart

--- src/main/java/benchmark/core/ResultChart.java
+++ src/main/java/benchmark/core/ResultChart.java
@@ -4,20 +4,34 @@ import javax.swing.JFrame;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.labels.StandardCategoryItemLabelGenerator;
 import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.renderer.category.BarRenderer;
 import org.jfree.data.category.DefaultCategoryDataset;
 
 public class ResultChart extends JFrame {
 
     public JFreeChart buildBarGraph(Long mergeTime, Long heapTime, Long bubbleTime) {
-            DefaultCategoryDataset bar = new DefaultCategoryDataset();
+        DefaultCategoryDataset bar = new DefaultCategoryDataset();
 
-            // Creating bars
-            bar.setValue(mergeTime, "MergeSort", "");
-            bar.setValue(heapTime, "HeapSort", "");
-            bar.setValue(bubbleTime, "BubbleSort", "");
+        // Creating bars
+        bar.setValue(mergeTime, "MergeSort", "");
+        bar.setValue(heapTime, "HeapSort", "");
+        bar.setValue(bubbleTime, "BubbleSort", "");
 
-            // Graph config
-            return ChartFactory.createBarChart("Tempo necessário (ms)", "", "ms", bar, PlotOrientation.HORIZONTAL, true, true, false);
+        // Create chart
+        JFreeChart chart = ChartFactory.createBarChart(
+            "Execution time (ms)", "", "",
+            bar,
+            PlotOrientation.HORIZONTAL,
+            true, true, false
+        );
+
+        // Enable value labels on bars
+        BarRenderer renderer = (BarRenderer) chart.getCategoryPlot().getRenderer();
+        renderer.setDefaultItemLabelGenerator(new StandardCategoryItemLabelGenerator());
+        renderer.setDefaultItemLabelsVisible(true);
+
+        return chart;
     }
 }
