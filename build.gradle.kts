1db02d23c09b38d2c4371b9335ca123ddb719700
Vitor Dias <vitorrenansouzadias@gmail.com>
2025-07-10 10:35:23 -0300

    update: set a default java version in Gradle build

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,10 @@ plugins {
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
+
 repositories {
     mavenCentral()
 }
@@ -12,7 +16,6 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
-
     implementation("org.jfree:jfreechart:1.5.5")
 }
 
